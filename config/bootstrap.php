d2b6ff05e4eda11d96f8bf9c4fcdbe802f7bddc3
Samih Soylu <samih.soylu@cm.com>
2023-08-28 18:50:57 +0200

    Add log error configuration

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 use function Sentry\init;
 
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+error_reporting(E_ALL);
+
 $_ENV['ROOT_DIR'] = dirname(__DIR__);
 $_ENV['APP_CONFIG_DIR'] = $_ENV['ROOT_DIR'] . '/config';
 $_ENV['LOG_DIR'] = $_ENV['ROOT_DIR'] . '/var/log';
